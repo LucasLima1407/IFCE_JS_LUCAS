872f290aeff93899c1b31ad19842cc92ac518fe4
Ant. Lucas Vieira de Lima <lucaslima14072003@gmail.com>
2023-02-28 15:23:16 -0300

    Aula278

---
+++ src/Aula09-Enum/enum.ts
@@ -0,0 +1,18 @@
+enum Cores {
+  VERMELHO = 200,
+  AZUL = 10,
+  AMARELO = 101,
+}
+enum Cores {
+  ROXO = "Roxo",
+}
+console.log(Cores.VERMELHO);
+console.log(Cores.AZUL);
+console.log(Cores.AMARELO);
+console.log(Cores[0]);
+console.log(Cores.ROXO);
+
+function escolhaACor(cor: Cores): void{
+  console.log(Cores[cor]);
+}
+escolhaACor(Cores.ROXO);
