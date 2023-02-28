604909718c1f805cf6a954c4d9ad57234a91106a
Ant. Lucas Vieira de Lima <lucaslima14072003@gmail.com>
2023-02-28 14:00:11 -0300

    Aula274

---
+++ src/Aula05-TipoArray/array.ts
@@ -0,0 +1,12 @@
+/* Array<T> ou T[] */
+function multiplyArgs(...args: Array<number>): number {
+  return args.reduce((ac, valor) => ac * valor, 1)
+}
+function concatenation(...args: string[]): string {
+  return args.reduce((string1, string2) => string1 + ' ' + string2)
+}
+function toUpperCase(...args: string[]): string {
+  return args.reduce((string1, string2) => string1.toUpperCase() + ' ' + string2.toUpperCase())
+}
+const resultado = toUpperCase('Eu só', 'quero é ser feliz', 'colocar')
+console.log(resultado)
