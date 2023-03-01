673195b7a0321629c8a22abd8eb527fad6c8b8fc
Ant. Lucas Vieira de Lima <lucaslima14072003@gmail.com>
2023-03-01 15:18:58 -0300

    Aula284

---
+++ src/Aula15-FunctionAsTypes/functiontype.ts
@@ -0,0 +1,16 @@
+type mapStringsCallback = (item: string) => string
+
+function mapStrings(array: string[], callbackfn: mapStringsCallback): string[] {
+  const newArray: string[] = [];
+
+  for (let i = 0; i < array.length; i++){
+    newArray.push(callbackfn(array[i]));
+  }
+
+  return newArray;
+}
+
+const abc = ["a", "b", "c"];
+const abcMapped = mapStrings(abc, (item) => item.toUpperCase());
+console.log(abc);
+console.log(abcMapped);
