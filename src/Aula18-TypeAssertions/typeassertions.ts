b4526da68fb4edd38ede0885a619ef8ac7fd94a5
IchBinLusc <lucaslima14072003@gmail.com>
2023-03-01 21:54:46 -0300

    Aula286

---
+++ src/Aula18-TypeAssertions/typeassertions.ts
@@ -0,0 +1,19 @@
+//Você pode ir funilando para que o ts reconheça que determinado elemento existe
+/* Recomendado */
+//Condicional
+const body1 = document.querySelector("body");
+if (body1) body1.style.background = "red";
+
+//Type asssertion
+const body3 = document.querySelector("body") as HTMLBodyElement;
+body3.style.background = "red";
+
+/* Não recomendado*/
+//Type assertion "errada", não podemos fzr direto o as number, então usamos unknown primeiro
+const body4 = document.querySelector("body") as unknown as number;
+console.log(body4);
+
+// Non-null assertion
+const body2 = document.querySelector("body")!;
+if (body2) body2.style.background = "red";
+
