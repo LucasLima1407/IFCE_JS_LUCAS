0b816b7f9d3769bafd35c1e0540cb615b34c7661
IchBinLusc <lucaslima14072003@gmail.com>
2023-03-07 18:43:25 -0300

    Aula299

---
+++ src/Aula26-GettersSetters/gettersAndsetters.ts
@@ -0,0 +1,27 @@
+export class Pessoa{
+  constructor(public nome: string, public sobrenome: string, private idade: number, protected cpf: string){}
+
+  set Nome(valor: string){
+    this.nome = valor;
+  }
+
+  get Nome(): string{
+    return this.nome;
+  }
+
+  get Idade(): number{
+    return this.idade;
+  }
+
+  get CPF(): string{
+    return this.cpf;
+  }
+
+  get NomeCompleto(): string {
+    return this.nome + " " + this.sobrenome;
+  }
+}
+const pessoa1 = new Pessoa("Lucas", "Lima", 19, "000.000.000-00");
+pessoa1.Nome = "Jonatha";
+console.log(pessoa1.Nome);
+console.log(pessoa1.CPF);
