88f285e6249a9b503c7b5b920b64ab87692f0cc2
IchBinLusc <lucaslima14072003@gmail.com>
2023-03-09 13:53:59 -0300

    Aula308

---
+++ src/Aula34-Interfaces/interfaces.ts
@@ -0,0 +1,32 @@
+// Interface propriamente dita
+interface TipoPessoa{
+  nome: string
+  sobrenome: string
+
+ nomeCompleto(): string
+}
+//Type alias servindo como interface
+type Numero = number | string
+
+type TipoPessoa2 = TipoPessoa
+
+export class Pessoa implements TipoPessoa2{
+  constructor(public nome: string,
+ public sobrenome: string) {}
+
+  nomeCompleto(): string{
+    return this.nome + " " + this.sobrenome;
+  }
+}
+
+const pessoaObj: TipoPessoa2 = {
+  nomeCompleto(){
+    return this.nome + " " + this.sobrenome;
+  },
+  nome: "Lucas",
+  sobrenome: "Lima"
+};
+
+const pessoa = new Pessoa("Lucas", "Lima");
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
