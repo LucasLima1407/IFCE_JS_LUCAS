8ea538bd3e5158f52ff605536694820bc71665a9
IchBinLusc <lucaslima14072003@gmail.com>
2023-03-11 12:02:39 -0300

    Aula311

---
+++ src/Aula36-TypeGuard/typeGuard.ts
@@ -0,0 +1,33 @@
+function add(a: unknown, b: unknown): number | string{
+  if(typeof a === "number" && typeof b === "number") return a + b;
+  return `${a} ${b}`;
+}
+
+console.log(add(1, 10));
+console.log(add("a","b"));
+
+type Pessoa = {tipo: "pessoa", nome: string};
+type Animal = {tipo: "animal", cor: string};
+
+type PessoaOuAnimal = Pessoa | Animal
+
+class Aluno implements Pessoa {
+  tipo: "pessoa" = "pessoa";
+  constructor(public nome: string) {}
+}
+
+function mostraNome(obj: PessoaOuAnimal): void{
+  //if("nome" in obj) ou
+  //if(obj instanceof Aluno) ou
+  switch(obj.tipo){
+  case "pessoa":
+    console.log(obj.nome);
+    return;
+  case "animal":
+    console.log(obj.cor);
+    return;
+  }
+}
+
+mostraNome(new Aluno("Lucas"));
+mostraNome({tipo: "animal", cor: "Rosa"});
